69bfeb84fb1375aab42c780fb7bb2cada2ddeaa1
ptokos <jiseokchoij@gmail.com>
2024-06-03 21:18:05 +0900

    [Feat] Carousel mouse-wheel-gesture 기능 및 버튼 누를 시 여러 슬라이드 이동하는 기능 추가

--- src/components/accommodation/category/CarouselCategory.tsx
+++ src/components/accommodation/category/CarouselCategory.tsx
@@ -10,22 +10,21 @@ import {
 } from '@/components/accommodation/category/ui/carousel'
 import { useState } from 'react'
 import Image from 'next/image'
-import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
 
 function ClickedButtonContent({ eachButton }) {
   return (
     <>
-      <div className="w-6 h-4 justify-center items-center inline-flex" />
+      <div className='inline-flex h-4 w-6 items-center justify-center' />
       <Image
-        className="opacity-75 group-hover:opacity-100"
+        className='opacity-75 group-hover:opacity-100'
         src={eachButton.src}
-        width="24"
-        height="24"
+        width='24'
+        height='24'
         alt={eachButton.contentName}
       />
 
-      <div className="flex-col justify-start items-start gap-4 flex ">
-        <p className="text-neutral-500 group-hover:text-black group-hover:underline underline-offset-[1rem] decoration-neutral-300 decoration-2 text-xs  font-semibold font-['SF Pro'] ">
+      <div className='flex flex-col items-start justify-start gap-4 '>
+        <p className="font-['SF Pro'] text-xs font-semibold text-neutral-500 decoration-neutral-300 decoration-2  underline-offset-[1rem] group-hover:text-black group-hover:underline ">
           {eachButton.contentName}
         </p>
       </div>
@@ -36,11 +35,16 @@ function ClickedButtonContent({ eachButton }) {
 function DefaultButtonContent({ eachButton }) {
   return (
     <>
-      <div className="w-6 h-4 justify-center items-center inline-flex" />
-      <Image className="opacity-100" src={eachButton.src} width="24" height="24" alt={eachButton.contentName}></Image>
+      <div className='inline-flex h-4 w-6 items-center justify-center' />
+      <Image
+        className='opacity-100'
+        src={eachButton.src}
+        width='24'
+        height='24'
+        alt={eachButton.contentName}></Image>
 
-      <div className="flex-col justify-start items-start gap-4 flex ">
-        <p className=" text-black underline underline-offset-[1rem] decoration-black decoration-2 text-xs  font-semibold font-['SF Pro'] ">
+      <div className='flex flex-col items-start justify-start gap-4 '>
+        <p className=" font-['SF Pro'] text-xs font-semibold text-black underline  decoration-black decoration-2 underline-offset-[1rem] ">
           {eachButton.contentName}
         </p>
       </div>
@@ -54,7 +58,7 @@ function CarouselEachButton({ clickedId, setIdClicked, eachButton }) {
       onClick={() => {
         setIdClicked(eachButton.id)
       }}
-      className=" p-4 group cursor-pointer pr-2 w-18 h-[8rem] flex-col justify-start items-center gap-2 inline-flex ">
+      className=' w-18 group inline-flex h-[8rem] cursor-pointer flex-col items-center justify-start gap-2 p-4 pr-2 '>
       {clickedId === eachButton.id ? (
         <DefaultButtonContent eachButton={eachButton} />
       ) : (
@@ -71,8 +75,12 @@ function CarouselEachButtonServer() {
     <>
       {category.map((eachButton, idx) => {
         return (
-          <CarouselItem key={idx} className="basis-1/7 flex items-center justify-center w-auto">
-            <CarouselEachButton clickedId={clickedId} setIdClicked={setIdClicked} eachButton={eachButton} />
+          <CarouselItem key={idx} className='basis-1/7 flex w-auto items-center justify-center'>
+            <CarouselEachButton
+              clickedId={clickedId}
+              setIdClicked={setIdClicked}
+              eachButton={eachButton}
+            />
           </CarouselItem>
         )
       })}
@@ -88,13 +96,13 @@ function CategoryCarousel() {
         skipSnaps: true,
         loop: false,
         dragFree: false,
+        watchDrag: false,
         axis: 'x',
         containScroll: 'trimSnaps',
       }}
-      plugins={[WheelGesturesPlugin({ forceWheelAxis: 'x', target: 'Element' })]}
-      className="w-full max-w-[70rem]">
-      <CarouselPrevious className="" />
-      <CarouselContent className="-ml-5">
+      className='w-full max-w-[70rem]'>
+      <CarouselPrevious />
+      <CarouselContent className='-ml-5'>
         <CarouselEachButtonServer />
       </CarouselContent>
       <CarouselNext />
