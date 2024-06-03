11d95919bf371ea11489f1eba9a153a2e0398657
ptokos <jiseokchoij@gmail.com>
2024-06-03 14:53:14 +0900

    [Fix] Button 컴포넌트 import 경로 수정

--- src/components/accommodation/category/ui/carousel.tsx
+++ src/components/accommodation/category/ui/carousel.tsx
@@ -5,7 +5,7 @@ import useEmblaCarousel, { type UseEmblaCarouselType } from 'embla-carousel-reac
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/accommodation/category/ui/button'
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
