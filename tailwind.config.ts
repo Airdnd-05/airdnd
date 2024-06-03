972e92ef9cd9ecc3894cf5e890a3a859a21326a6
seoeunhyo <seoeunhyo99@gmail.com>
2024-06-03 14:54:16 +0900

    [Design] 호스트 상세 정보 컨테이너 커스텀 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        'host-detail-gray': '#f0efe9',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
