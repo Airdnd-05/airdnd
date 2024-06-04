10b55dec436ab418e86647e64414ebb330bd9922
kiwisae99 <judyshin99@gmail.com>
2024-06-04 17:47:03 +0900

    [Feat] Detail Page에 Calendar 추가

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -2,6 +2,7 @@ import DetailPageBanner from '@/components/accommodation/detailBanner/DetailPage
 import HostDetail from '@/components/accommodation/hostDetail/HostDetailContainer'
 import InfoWrapper from '@/components/accommodation/infoWrapper/InfoWrapper'
 import AmenityInfo from '@/components/accommodation/amenityInfo/AmenityInfo'
+import Calendar from '@/components/calendar/calendar'
 
 function AccommodationPage({ params }) {
   const { id } = params
@@ -11,6 +12,7 @@ function AccommodationPage({ params }) {
       <DetailPageBanner id={id} />
       <InfoWrapper id={id} />
       <AmenityInfo id={id} />
+      <Calendar months={3} />
       <HostDetail id={id} />
     </>
   )
