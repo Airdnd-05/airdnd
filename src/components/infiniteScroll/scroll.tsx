387ef4a01402b4caadf2bcf4d9c9cf3b469d406e
seoeunhyo <seoeunhyo99@gmail.com>
2024-06-08 22:18:04 +0900

    [Feat] 스크롤시 가져오는 카드의 개수를 20개로 변경

--- src/components/infiniteScroll/scroll.tsx
+++ src/components/infiniteScroll/scroll.tsx
@@ -4,7 +4,6 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import Card from '@/components/card/card'
 import Link from 'next/link'
 import getRoomList from '@/app/apis/fetchMainPage/getRoomsList'
-import Loader from '@/components/infiniteScroll/Loader'
 
 function RoomsItem({ accommodation }) {
   return (
@@ -26,29 +25,28 @@ function MainInfiniteScroll() {
   const [id, setId] = useState(21)
   const loaderRef = useRef(null)
 
-  const fields = [
-    //useMemo
-    'accommodationId',
-    'accommodationName',
-    'imageUrl',
-    'guestFavorite',
-    'rating',
-    'pricePerDay',
-  ]
-
   const fetchAccommodation = useCallback(async () => {
-    if (id >= 100) return
+    if (id > 340) return
     if (isLoading) return
+    const fields = [
+      'accommodationId',
+      'accommodationName',
+      'imageUrl',
+      'guestFavorite',
+      'rating',
+      'pricePerDay',
+    ]
 
     setIsLoading(true)
 
     const newAccommodation = await getRoomList(id, fields)
+
     setAccommodations(prev => [...prev, ...newAccommodation])
 
     setId(prevId => prevId + 20)
 
     setIsLoading(false)
-  }, [id, isLoading, fields])
+  }, [id, isLoading])
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -79,13 +77,13 @@ function MainInfiniteScroll() {
   }, [fetchAccommodation])
 
   return (
-    <div className='mt-8 grid w-full max-w-[1760px] grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+    <>
       {accommodations.map((accommodation, index) => (
         <RoomsItem key={`RoomsItem-${index}`} accommodation={accommodation} />
       ))}
       <div></div>
-      <div ref={loaderRef}>{isLoading && <Loader />}</div>
-    </div>
+      <div ref={loaderRef}></div>
+    </>
   )
 }
 
