4bf4644404e8730b37f8a3e61f4369b1a6715a53
seoeunhyo <seoeunhyo99@gmail.com>
2024-06-08 21:58:47 +0900

    [Fix] 무한 스크롤이 아래로 늘어나지 않고 같은 사이즈 내 갱신만 되던 버그 해결

--- src/components/infiniteScroll/scroll.tsx
+++ src/components/infiniteScroll/scroll.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import Card from '@/components/card/card'
 import Link from 'next/link'
 import getRoomList from '@/app/apis/fetchMainPage/getRoomsList'
+import Loader from '@/components/infiniteScroll/Loader'
 
 function RoomsItem({ accommodation }) {
   return (
@@ -22,9 +23,11 @@ function RoomsItem({ accommodation }) {
 function MainInfiniteScroll() {
   const [accommodations, setAccommodations] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  const [id, setId] = useState(41)
+  const [id, setId] = useState(21)
   const loaderRef = useRef(null)
+
   const fields = [
+    //useMemo
     'accommodationId',
     'accommodationName',
     'imageUrl',
@@ -32,44 +35,56 @@ function MainInfiniteScroll() {
     'rating',
     'pricePerDay',
   ]
+
   const fetchAccommodation = useCallback(async () => {
+    if (id >= 100) return
     if (isLoading) return
 
     setIsLoading(true)
 
-    setAccommodations(await getRoomList(id, fields))
+    const newAccommodation = await getRoomList(id, fields)
+    setAccommodations(prev => [...prev, ...newAccommodation])
 
-    setId(prevId => prevId + 1)
+    setId(prevId => prevId + 20)
 
     setIsLoading(false)
-  }, [id, isLoading])
+  }, [id, isLoading, fields])
 
   useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      const target = entries[0]
-      if (target.isIntersecting) {
-        fetchAccommodation()
-      }
-    })
+    const observer = new IntersectionObserver(
+      entries => {
+        const target = entries[0]
 
-    if (loaderRef.current) {
-      observer.observe(loaderRef.current)
+        if (target.isIntersecting) {
+          fetchAccommodation()
+        }
+      },
+      {
+        threshold: 0.5,
+        rootMargin: '100px',
+      },
+    )
+
+    const currentLoader = loaderRef.current
+
+    if (currentLoader) {
+      observer.observe(currentLoader)
     }
 
     return () => {
-      if (loaderRef.current) {
-        observer.unobserve(loaderRef.current)
+      if (currentLoader) {
+        observer.unobserve(currentLoader)
       }
     }
   }, [fetchAccommodation])
 
   return (
-    <div className='container'>
+    <div className='mt-8 grid w-full max-w-[1760px] grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
       {accommodations.map((accommodation, index) => (
         <RoomsItem key={`RoomsItem-${index}`} accommodation={accommodation} />
       ))}
-
-      <div ref={loaderRef}></div>
+      <div></div>
+      <div ref={loaderRef}>{isLoading && <Loader />}</div>
     </div>
   )
 }
