0dd346a0f1c4bc6378b1752a1c2b02b47fffdc23
ptokos <jiseokchoij@gmail.com>
2024-06-10 20:31:29 +0900

    [Refator] carousel.json 변수명 수정에 따른 변수명 수정

--- src/components/accommodation/categoryBundle/Category.tsx
+++ src/components/accommodation/categoryBundle/Category.tsx
@@ -18,15 +18,15 @@ function ClickedButtonContent({ eachButton }) {
       <div className='inline-flex h-4 w-6 items-center justify-center' />
       <Image
         className='opacity-75 group-hover:opacity-100'
-        src={eachButton.src}
+        src={eachButton.categoryIcon}
         width='24'
         height='24'
-        alt={eachButton.contentName}
+        alt={eachButton.categoryName}
       />
 
       <div className='flex flex-col items-start justify-start gap-4 '>
         <p className="font-['SF Pro'] text-xs font-semibold text-neutral-500 decoration-neutral-300 decoration-2  underline-offset-[1rem] group-hover:text-black group-hover:underline ">
-          {eachButton.contentName}
+          {eachButton.categoryName}
         </p>
       </div>
     </>
@@ -39,14 +39,14 @@ function DefaultButtonContent({ eachButton }) {
       <div className='inline-flex h-4 w-6 items-center justify-center' />
       <Image
         className='opacity-100'
-        src={eachButton.src}
+        src={eachButton.categoryIcon}
         width='24'
         height='24'
-        alt={eachButton.contentName}></Image>
+        alt={eachButton.categoryName}></Image>
 
       <div className='flex flex-col items-start justify-start gap-4 '>
         <p className=" font-['SF Pro'] text-xs font-semibold text-black underline  decoration-black decoration-2 underline-offset-[1rem] ">
-          {eachButton.contentName}
+          {eachButton.categoryName}
         </p>
       </div>
     </>
@@ -57,10 +57,10 @@ function CarouselEachButton({ clickedId, setIdClicked, eachButton }) {
   return (
     <button
       onClick={() => {
-        setIdClicked(eachButton.id)
+        setIdClicked(eachButton.categoryId)
       }}
       className=' w-18 group inline-flex h-[8rem] cursor-pointer flex-col items-center justify-start gap-2 p-4 pr-2 '>
-      {clickedId === eachButton.id ? (
+      {clickedId === eachButton.categoryId ? (
         <DefaultButtonContent eachButton={eachButton} />
       ) : (
         <ClickedButtonContent eachButton={eachButton} />
