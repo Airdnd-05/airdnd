8137aafa8e00644b16e598aa47ae2090849d5026
kiwisae99 <judyshin99@gmail.com>
2024-06-12 20:19:31 +0900

    [Design] 디테일 페이지 내 CSS 수정

--- src/components/accommodation/amenityInfo/AmenityInfo.tsx
+++ src/components/accommodation/amenityInfo/AmenityInfo.tsx
@@ -22,7 +22,7 @@ async function AmenityInfo({ id }) {
   const { amenities } = await getRoomsDetail(id, ['amenities'])
   return (
     <div className='border-b border-solid border-neutral-300 py-12'>
-      <h2 className='mb-4 text-xl font-bold'>숙소 편의시설</h2>
+      <h2 className='mb-4 text-[22px] font-semibold'>숙소 편의시설</h2>
       <div className='grid grid-cols-2 gap-4'>
         {amenities.map((amenity, index) => (
           <div key={`AmenityItem-${index}`}>
